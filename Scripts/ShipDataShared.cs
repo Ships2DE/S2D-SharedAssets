b15aba4b11ec0c1e8bca8cb0e5a386fe931398ee
Hannes <hannes1652@web.de>
2023-11-17 22:17:34 +0100

    shared scripts

---
+++ Scripts/ShipDataShared.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public class ShipDataShared : Node {
+
+	[Export] public int Id;
+	[Export] public string Classname;
+	[Export] public float Speed, TurnSpeed, ReverseMulti;
+	
+
+}
